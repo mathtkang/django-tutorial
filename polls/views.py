06aacbb12948f13b11a6f6543a4becd4815870f8
[2기-1반] 강석영 <ksge1124@gmail.com>
2022-12-02 19:06:51 +0900

    using template

--- polls/views.py
+++ polls/views.py
@@ -5,12 +5,12 @@ from .models import Question
 
 
 def index(request):
-    # 시스템에 저장된 최소한 5 개의 투표 질문이 콤마로 분리되어, 발행일에 따라 출력
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
-    output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list,
+    }  # a dictionary mapping template variable names to Python objects
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
@@ -22,3 +22,12 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
+
+
+
+
+
+def index(request):
+    
+    
+    
